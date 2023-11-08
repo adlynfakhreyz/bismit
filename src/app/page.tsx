dddde56170851e269fcce5f436df98b7a60edc30
adlynfakhreyz <adlynfakhreyz17@gmail.com>
2023-11-08 16:52:20 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-white">
+      <nav className="bg-white w-full h-20 shadow-lg flex px-6 py-3">
+        <div className="flex flex-row justify-center">
+          <button>Homepage</button>
+          <button>Friends</button>
+          <button>Tasks</button>
+          <button>Events</button>
+          <button>About Fasilkom/UI</button>
+        </div>
+      </nav>
+    </main>
+  )
+}
