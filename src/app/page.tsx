ef12e9bcd23c237002f07add1cb6f03f4d366d78
adlynfakhreyz <adlynfakhreyz17@gmail.com>
2023-11-22 15:51:20 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,51 +25,69 @@ export default function home() {
         <div className= 'h-full w-full'>
           <div className='relative'>
             <img className = 'h-full w-full' src = "https://i.ibb.co/LC9qDcF/snazzy-image-3-1.png"></img>
-            <div className='absolute'style={{ bottom: '160px', left: '600px'}}>
-              <img
-                className='h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
-                src='https://i.ibb.co/BCxWDL3/Frame-1032.png' alt="Overlay Image Dallas"style={{ zIndex: 10 }}/>
-            </div>
-            <div className = 'absolute'style={{ top: '425px', left: '500px'}}>
-              <img
-                className= 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
-                src='https://i.ibb.co/Q8frhhH/Frame-1031.png'alt="Overlay Image Kantek" style={{ zIndex: 10 }}/>
-            </div> 
-            <div className = 'absolute'style={{ top: '300px', left: '600px'}}>
-              <img
+            <a href='/reviews'>
+              <div className='absolute'style={{ bottom: '160px', left: '600px'}}>
+                <img
+                  className='h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
+                  src='https://i.ibb.co/BCxWDL3/Frame-1032.png' alt="Overlay Image Dallas"style={{ zIndex: 10 }}/>
+              </div>
+            </a>
+            <a href='/reviews'>
+              <div className = 'absolute'style={{ top: '425px', left: '500px'}}>
+                <img
+                  className= 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
+                  src='https://i.ibb.co/Q8frhhH/Frame-1031.png'alt="Overlay Image Kantek" style={{ zIndex: 10 }}/>
+              </div> 
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute'style={{ top: '300px', left: '600px'}}>
+                <img
+                  className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90' 
+                  src='https://i.ibb.co/YLQ9DL0/Frame-1034.png' alt="Overlay Image Kafe" style={{ zIndex: 10 }}/>
+              </div>
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute'style={{ bottom: '200px', left: '330px'}}>
+                <img 
+                className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
+                src='https://i.ibb.co/4wqxZkx/Frame-1035.png' alt="Overlay Image Kanvok" style={{ zIndex: 10 }}/>
+              </div> 
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute'style={{ top: '480px', left: '700px'}}>
+                <img 
+                className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
+                src='https://i.ibb.co/G3VGWPP/Frame-1036.png' alt="Overlay Image Balgebun" style={{ zIndex: 10 }}/>
+              </div> 
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute'style={{ top: '470px', right: '520px'}}>
+                <img 
+                className ='h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
+                src='https://i.ibb.co/RzpNbVX/Frame-1038.png' alt="Overlay Image Kanmer" style={{ zIndex: 10 }}/>
+              </div> 
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute'style={{ top: '460px', left: '625px'}}>
+                <img 
                 className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90' 
-                src='https://i.ibb.co/YLQ9DL0/Frame-1034.png' alt="Overlay Image Kafe" style={{ zIndex: 10 }}/>
-            </div>
-            <div className = 'absolute'style={{ bottom: '200px', left: '330px'}}>
-              <img 
-              className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
-              src='https://i.ibb.co/4wqxZkx/Frame-1035.png' alt="Overlay Image Kanvok" style={{ zIndex: 10 }}/>
-            </div> 
-            <div className = 'absolute'style={{ top: '480px', left: '700px'}}>
-              <img 
-              className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
-              src='https://i.ibb.co/G3VGWPP/Frame-1036.png' alt="Overlay Image Balgebun" style={{ zIndex: 10 }}/>
-            </div> 
-            <div className = 'absolute'style={{ top: '470px', right: '520px'}}>
-              <img 
-              className ='h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90'
-              src='https://i.ibb.co/RzpNbVX/Frame-1038.png' alt="Overlay Image Kanmer" style={{ zIndex: 10 }}/>
-            </div> 
-            <div className = 'absolute'style={{ top: '460px', left: '625px'}}>
+                src='https://i.ibb.co/pjd7n3Z/Frame-1041.png'alt="Overlay Image Kansas" style={{ zIndex: 10 }}/>
+              </div>
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute'style={{ bottom: '170px', right: '555px'}}>
+                <img 
+                className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90' 
+                src='https://i.ibb.co/jytt0C0/Frame-1039.png'alt="Overlay Image Kanrik" style={{ zIndex: 10 }}/>
+              </div>
+            </a>
+            <a href="/reviews">
+              <div className = 'absolute' style={{top: '430px', right: '600px'}}>
               <img 
               className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90' 
-              src='https://i.ibb.co/pjd7n3Z/Frame-1041.png'alt="Overlay Image Kansas" style={{ zIndex: 10 }}/>
-            </div>
-            <div className = 'absolute'style={{ bottom: '170px', right: '555px'}}>
-              <img 
-              className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90' 
-              src='https://i.ibb.co/jytt0C0/Frame-1039.png'alt="Overlay Image Kanrik" style={{ zIndex: 10 }}/>
-            </div>
-            <div className = 'absolute' style={{top: '430px', right: '600px'}}>
-            <img 
-            className = 'h-24 w-20 transition-transform transform hover:scale-125 hover:opacity-90' 
-            src='https://i.ibb.co/8Bb6Dph/Frame-1033.png'alt="Overlay Image Takor" style={{ zIndex: 10 }}/>
-            </div>
+              src='https://i.ibb.co/8Bb6Dph/Frame-1033.png'alt="Overlay Image Takor" style={{ zIndex: 10 }}/>
+              </div>
+            </a>
           </div>
         </div>
         {/* Your main content */}
